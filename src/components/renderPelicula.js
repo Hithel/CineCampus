067bba55685ff19ca99f054f8a7aa4f330811707
Hithel <Edward22cd@gmail.com>
2023-06-06 20:40:25 -0500

    feat: :sparkles: Solucion de Bugs
    Solucion de Bugs
    
    Solucion de Bugs

--- src/components/renderPelicula.js
+++ src/components/renderPelicula.js
@@ -10,11 +10,12 @@ export default function renderPelicula(data) {
         div.setAttribute("style","width: 18rem;");
 
         div.innerHTML = `
+            <img src="${peli.imagen}" class="card-img-top">
             <div class="card-body" id="${peli.id}">
                 <h5 class="card-title">${peli.titulo}</h5>
                 <p class="card-text">${peli.genero}</p>
-                <p class="card-text">${peli.genero}</p>
-                <p class="card-text">${peli.genero}</p>
+                <p class="card-text">${peli.duracion}</p>
+                <p class="card-text">${peli.director}</p>
                 <input type="submit" data-accion="Eliminar" value="Eliminar" class="btn-guardar bg-danger border-0 rounded bg-secondary px-2">
                 <input type="button" data-bs-toggle="modal" data-bs-target="#modalModificar"  data-accion="Actualizar" value="Actualizar" class="btn-guardar bg-warning border-0 rounded bg-secondary px-2">
             </div>
